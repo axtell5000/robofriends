ea7e8dfb3bf33ae26659dd0e81fe501fec518b92
Stephen Axtell <axtell.stephen@gmail.com>
2018-11-06 12:18:34 +0200

    added redux and redux thunk

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,21 @@
-import { CHANGE_SEARCH_FIELD } from './constants';
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOT_PENDING,
+  REQUEST_ROBOT_SUCCESS,
+  REQUEST_ROBOT_FAILED
+} from './constants';
 
 
 export const setSearchField = (text) => ({
   type: CHANGE_SEARCH_FIELD,
   payload: text
 });
+
+// higher order function, returning a function
+export const requestRobots = () => (dispatch) => {
+  dispatch({ type: REQUEST_ROBOT_PENDING});
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=> response.json())
+    .then(data => dispatch({ type: REQUEST_ROBOT_SUCCESS, payload: data}))
+    .catch(error => dispatch({ type: REQUEST_ROBOT_FAILED, payload: error}));
+};
--- src/reducers.js
+++ src/reducers.js
@@ -1,15 +1,41 @@
-import { CHANGE_SEARCH_FIELD } from './constants';
+import {
+  CHANGE_SEARCH_FIELD,
+  REQUEST_ROBOT_PENDING,
+  REQUEST_ROBOT_SUCCESS,
+  REQUEST_ROBOT_FAILED
+} from './constants';
 
-const initialState = {
+
+const initialStateSearch = {
   searchField: ''
 };
 
 // Reducers take an action and interact with the state. State is read only. When changing we are making copies
-export const searchRobots = (state = initialState, action = {}) => {
+export const searchRobots = (state = initialStateSearch, action = {}) => {
   switch(action.type) {
     case CHANGE_SEARCH_FIELD:
       // assign - making a copy of object. The spread operator ... does the same thing as assign
       return Object.assign({}, state, {searchField: action.payload});
+    default:
+      return state;
+  }
+};
+
+const initialStateRobots = {
+  isPending: false,
+  robots: [],
+  error: ''
+};
+
+export const requestRobots = (state = initialStateRobots, action = {}) => {
+  switch(action.type) {
+    case REQUEST_ROBOT_PENDING:
+      // assign - making a copy of object. The spread operator ... does the same thing as assign
+      return Object.assign({}, state, {isPending: true});
+    case REQUEST_ROBOT_SUCCESS:
+      return Object.assign({}, state, {robots: action.payload, isPending: false});
+    case REQUEST_ROBOT_FAILED:
+      return Object.assign({}, state, {error: action.payload, isPending: false});
     default:
       return state;
   }
